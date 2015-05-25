ac2ca90df2e937321212849cb17f581b9bf00f1a
miguel <miguel@106-01.info2>
2015-05-25 09:32:29 +0200

    Clase HolaMundo recien creada

---
+++ src/mig/HolaMundo.java
@@ -0,0 +1,14 @@
+package mig;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
